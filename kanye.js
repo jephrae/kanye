986cb5ec1d04371e8cf55897a50f0b9a8f35e6ac
jephrae <jeff.simons@gmail.com>
2020-12-17 17:36:45 -0600

    abcs

---
+++ kanye.js
@@ -0,0 +1,28 @@
+//string storage
+const strings = [
+    ['I wish I had a friend like me'],
+    ['My dad got me a drone for Christmas'],
+    ['Fur pillows are hard to actually sleep on'],
+    ['If I got any cooler I would freeze to death']
+]
+console.log(strings)
+
+//alphabet buttons
+
+
+const abcArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+console.log(abcArray)
+
+
+//populating sentence spaces
+const stringSpaces = 0
+
+
+
+
+//alphabet button click functionality
+
+
