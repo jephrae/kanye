8ead91b09020ac4d629db439a01c688edf4df8ab
jephrae <jeff.simons@gmail.com>
2020-12-18 11:10:32 -0600

    buttonz

--- kanye.js
+++ kanye.js
@@ -17,14 +17,20 @@ const abcArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-
 const abcButtons = document.getElementsByClassName('abcButtons')
 
+function printBtn() {
+    for (let i = 0; i < abcArray.length; i++) {
+       let btn = document.createElement("button");
+       let t = document.createTextNode(abcArray[i]);
+       btn.appendChild(t);
+       document.body.appendChild(btn);
+    }
+}
+
+printBtn()
 
 
-// const buttons = makeABC() {
-//     abcButtons = document.getElementById('#abcButtons')
-// }
 
 console.log(abcButtons)
 
