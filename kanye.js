18529509ba71bd2810ee4d31711c1bbc87c197fb
jephrae <jeff.simons@gmail.com>
2020-12-19 10:01:30 -0600

    work?

--- kanye.js
+++ kanye.js
@@ -5,29 +5,52 @@ const kanyeStrings = [
     ['Fur pillows are hard to actually sleep on'],
     ['If I got any cooler I would freeze to death']
 ]
+const abcArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+//getting random string from array string storage
 
 let randomString = kanyeStrings[Math.floor(Math.random() * kanyeStrings.length)];
 console.log(randomString)
+// taking the characters out of random string, with the spaces
+
+
+//making blanks using array for each make a blank item on HTML
+const stringBlanks = document.getElementsByClassName('blankString')
+console.log(stringBlanks)
+
+function printBlanks() {  //need to change for argument to point to spiced array made from randomstring
+    for (let i = 0; i < abcArray.length; i++) {
+       let blank = document.createElement("_");
+       let t = document.createTextNode(randomString);
+       blank.appendChild(t);
+       document.body.appendChild(blank);
+    }
+}
+printBlanks()
+
+
+
+
+
 
 
 
 //alphabet buttons
 
-const abcArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 const abcButtons = document.getElementsByClassName('abcButtons')
 
 function printBtn() {
     for (let i = 0; i < abcArray.length; i++) {
-       let btn = document.createElement("button");
+       let button = document.createElement("button");
        let t = document.createTextNode(abcArray[i]);
-       btn.appendChild(t);
-       document.body.appendChild(btn);
+       button.appendChild(t);
+       document.body.appendChild(button);
     }
 }
-
 printBtn()
 
 
