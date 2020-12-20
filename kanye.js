6819880f59b5e0206426847e0847acca26619d48
jephrae <jeff.simons@gmail.com>
2020-12-20 14:27:59 -0600

    ugh restart

--- kanye.js
+++ kanye.js
@@ -11,59 +11,66 @@ const abcArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//getting random string from array string storage
-let randomString = kanyeStrings[Math.floor(Math.random() * kanyeStrings.length)];
-console.log(randomString)
-
-//lives counter
-let lives = 7
-
-//showing random string
-// const stringBlanks = document.getElementsByClassName('blankString')
-// console.log(stringBlanks)
-
-function printBlanks() {  //need to change for argument to point to spiced array made from randomstring
-    for (let i = 0; i < randomString.length; i++) {
-       let blank = document.createElement("h2");
-       let blanks = document.createTextNode(randomString);
-       blank.appendChild(blanks);
-       document.body.appendChild(blank);
-    }
-}
-printBlanks()
+// //lives counter
+// let lives = 7
 
 
-//alphabet buttons
-const abcButtons = document.getElementsByClassName('abcButtons')
+// //getting random string from array string storage
+// let randomString = kanyeStrings[Math.floor(Math.random() * kanyeStrings.length)];
+// console.log(randomString)
 
-function printBtn() {
-    for (let i = 0; i < abcArray.length; i++) {
-       let button = document.createElement("button");
-       let t = document.createTextNode(abcArray[i]);
-       button.id = abcArray[i];
-       button.appendChild(t);
-       document.body.appendChild(button)
-       document.getElementById;
-    }
-}
-printBtn()
+// //showing random string
+// // const stringBlanks = document.getElementsByClassName('blankString')
+// // console.log(stringBlanks)
 
-
-console.log(abcButtons)
-
-//making buttons listen for and select items from randomString on click
+// function printBlanks() {  //need to change for argument to point to spiced array made from randomstring
+//     for (let i = 0; i < randomString.length; i++) {
+//        let blank = document.createElement("h2");
+//        let blanks = document.createTextNode(randomString);
+//        blank.appendChild(blanks);
+//        document.body.appendChild(blank);
+//     }
+// }
+// printBlanks()
 
 
+// //alphabet buttons
+// const abcButtons = document.getElementsByClassName('abcButtons')
+
+// function printBtn() {
+//     for (let i = 0; i < abcArray.length; i++) {
+//        let button = document.createElement("button");
+//        let t = document.createTextNode(abcArray[i]);
+//        button.id = abcArray[i];
+//        button.appendChild(t);
+//        document.body.appendChild(button)
+//        document.getElementById;
+//     }
+// }
+// printBtn()
+
+
+// console.log(abcButtons)
+
+// //making buttons listen for and select items from randomString on click
+
+// // abcButtons.addEventListener('click',()=> {
+// //     if (lives >0){  
+// //         lives -=1;
+// // console.log(lives)
+// //     }else if (lives == 0)
+// //     console.log(gameOver)
+// // })
 
 
 
-//populating sentence spaces
-const stringSpaces = 0
+// //populating sentence spaces
+// const stringSpaces = 0
 
 
 
 
-//alphabet button click functionality
+// //alphabet button click functionality
 
 
 //turn counter for now
